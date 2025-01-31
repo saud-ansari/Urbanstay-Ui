aa031c5106cfd8e6b3e6f782db58fb837e48350d
sayyed faizan <sayyedfaizan1777@outlook.com>
2025-01-31 20:58:59 +0530

    dashboard +

--- src/components/Charts/AdminCharts/PropertyBarChart.js
+++ src/components/Charts/AdminCharts/PropertyBarChart.js
@@ -3,36 +3,18 @@ import React, { useEffect, useState } from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const PropertyBarChart = () => {
-  const [dataInfo, setDataInfo] = useState([]); // Initialize as an empty array
-  const [data , setData] = useState({
-    propertyType: 'studio',
-    value: 10,
-    count: 10
-  })
+  
+  const [data, setData] = useState([]);
 
-  // Fetch data from the API
-  // useEffect(() => {
-  //   axios.get('http://localhost:57614/api/Property')
-  //     .then((response) => {
-  //       if (response.data) {
-  //         setDataInfo(response.data); // Set the fetched data to the state
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       console.error('Error fetching data: ', error);
-  //     });
-  // }, []); 
-  // // Empty dependency array ensures the effect runs only once
-
-  // useEffect(() => {
-  //   if (dataInfo.length > 0) {
-  //     const studioCount = dataInfo.filter(item => item.propertyType === "Studios").count;
-  //     setData([
-  //       { propertyType: 'Studios', count: studioCount },
-  //       // You can add other properties similarly based on the fetched data
-  //     ]);
-  //   }
-  // }, [dataInfo]); // Run this effect when dataInfo changes
+  useEffect(() => {
+    axios.get('http://localhost:57614/PropertyCountType')
+    .then(res =>{
+      setData(res.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  },[]);
 
   return (
     <div>
@@ -41,11 +23,11 @@ const PropertyBarChart = () => {
         <BarChart data={data}> {/* Use dataInfo here */}
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="propertyType" />
-          <YAxis />
+          <YAxis dataKey="count" />
           <Tooltip />
           <Legend />
-          <Bar dataKey="listed" fill="#8884d8" />
-          <Bar dataKey="rented" fill="#82ca9d" />
+          <Bar dataKey="propertyType" fill="propertyType" />
+          <Bar dataKey="count" fill="#82ca9d" />
         </BarChart>
       </ResponsiveContainer>
     </div>
